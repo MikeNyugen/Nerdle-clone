3b84bf09cbf306fb3e6ecb30cb110fabdb8d53cb
Michael Clark <mac37@st-andrews.ac.uk>
2022-02-27 19:41:17 +0000

    added game tests

--- numble/src/test/java/com/numble/ModelTests/GameTest.java
+++ numble/src/test/java/com/numble/ModelTests/GameTest.java
@@ -1,13 +1,47 @@
 package com.numble.ModelTests;
 
+import com.numble.model.Equation;
+import com.numble.model.Game;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GameTest {
 
   @Test
-  void testSomthing() {
-    assertEquals(1,1);
+  void gameCreationTest() {
+    Game game = new Game();
+    assertNotNull(game);
   }
+
+  @Test
+  void checkGuessTest() {
+    Game game = mock(Game.class);
+    when(game.getTarget()).thenReturn("12+6=18");
+    assertEquals("[0,3,0,3,0,0,0]", game.checkGuess("11+5=18").toString());
+    assertEquals(game.checkGuess("[2,2,0,3,0,3]"), game.checkGuess("81+9=90").toString());
+  }
+
+  @Test
+  void equationGeneratorTest() {
+    Game game = mock(Game.class);
+    assertNotNull(game.generateEquation());
+  }
+
+  @Test
+  void hasWonTest() {
+    Game game = mock(Game.class);
+    
+    when(game.getTarget()).thenReturn("11+1=13");
+    assertFalse(game.hasWon());
+
+    game.checkGuess("11+1=14");
+    assertFalse(game.hasWon());
+
+    game.checkGuess("11+1=13");
+    assertTrue(game.hasWon());
+  }
+
 }
