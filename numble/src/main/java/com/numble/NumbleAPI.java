a204650c0cfc1f4e62e5740434f4caa51aa99e60
Dmitry Voronetskiy <davoronetskiy@gmail.com>
2022-03-01 09:42:17 +0000

    developing API

--- numble/src/main/java/com/numble/NumbleAPI.java
+++ numble/src/main/java/com/numble/NumbleAPI.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.numble.model.Game;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,43 @@ public class NumbleAPI {
         result.put("game_id", nextGameID);
         games.put(nextGameID, new Game());
         nextGameID++;
+        return result;
+    }
+
+    @GetMapping("/get_target/{id}")
+    public ObjectNode getTarget(@PathVariable int id) {
+        ObjectMapper mapper = new ObjectMapper();
+        var result = mapper.createObjectNode();
+        if (id < nextGameID) {
+            result.put("status", "OK");
+            result.put("target", games.get(id).getTarget());
+        } else {
+            result = makeError(404, "invalid game id");
+        }
+        return result;
+    }
+
+    private ObjectNode makeError(int code, String message) {
+        ObjectMapper mapper = new ObjectMapper();
+        var result = mapper.createObjectNode();
+        result.put("status", "error");
+        var error = mapper.createObjectNode();
+        error.put("code", code);
+        error.put("description", message);
+        result.set("error", error);
+        return result;
+    }
+
+    @GetMapping("/has_won/{id}")
+    public ObjectNode hasWon(@PathVariable int id) {
+        ObjectMapper mapper = new ObjectMapper();
+        var result = mapper.createObjectNode();
+        if (id < nextGameID) {
+            result.put("status", "OK");
+            result.put("has_won", games.get(id).hasWon());
+        } else {
+            result = makeError(404, "invalid game id");
+        }
         return result;
     }
 }
