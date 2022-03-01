89406c0d00cf5b63248b406f002f0639021534ab
Mike Nguyen <mikenguyen017@gmail.com>
2022-03-01 15:45:55 +0000

    Decomposed 'checkGuess' method into smaller functions

--- numble/src/main/java/com/numble/model/Game.java
+++ numble/src/main/java/com/numble/model/Game.java
@@ -7,72 +7,85 @@ import java.util.Arrays;
 
 public class Game implements GameInterface {
 
-  //attributes
+  // attributes
   private String target = null;
   private String targetResult = null;
   private ArrayList<Integer> colourCode;
-  private Equation.Pair equationResultPair;
+  private Equation.Pair<String, String> equationResultPair;
+  private ArrayList<String> remainingCharList;
 
-  //methods
+  // methods
   public Game() {
     equationResultPair = Equation.getEquationResultPair();
     target = (String) equationResultPair.getEquation();
     targetResult = (String) equationResultPair.getResult();
-    colourCode = new ArrayList<Integer>();
+    remainingCharList = new ArrayList<>();
   }
 
   public Game(String target, String result) {
     this.target = target;
     this.targetResult = result;
-    colourCode = new ArrayList<Integer>();
+    remainingCharList = new ArrayList<>();
   }
 
   @Override
   public ArrayList<Integer> checkGuess(ArrayList<String> userGuessArray) {
-
-    //The target as an array list to compare against the userGuessArray
+    // The target as an array list to compare against the userGuessArray
     String[] targetArray = target.split("");
     ArrayList<String> targetList = new ArrayList<>(Arrays.asList(targetArray));
+    colourCode = new ArrayList<Integer>();
 
-    //The colour code to return
-  
-    //Initialize colour code so that it is the length of the target, default values being 2
+    initilizeColours(targetList);
+    setGreenTiles(userGuessArray, targetList);
+    setOrangeTiles(userGuessArray, targetList);
+
+    return colourCode;
+  }
+
+  void initilizeColours(ArrayList<String> targetList) {
+    // Initialize colour code so that it is the length of the target, default values
+    // being 2
     for (int i = 0; i < targetList.size(); i++) {
       colourCode.add(2);
     }
-    System.out.println("INIT CC " + colourCode);
+  }
 
-    //Loop through the guess to identify indexes which are correct (change in colour code), and add any which aren't exactly correct to remaining character list
-    ArrayList<String> remainingCharList = new ArrayList<>();
-    for (int i = 0; i<userGuessArray.size(); i++) {
-      if (userGuessArray.get(i).equals(targetList.get(i))) {
+  void setGreenTiles(ArrayList<String> userGuessArray, ArrayList<String> targetList) {
+    // Loop through the guess to identify indexes which are correct (change in
+    // colour code), and add any which aren't exactly correct to remaining character
+    // list
+    for (int i = 0; i < userGuessArray.size(); i++) {
+      String number = userGuessArray.get(i);
+      String targetNumber = targetList.get(i);
+      if (number.equals(targetNumber)) {
         colourCode.set(i, 0);
         targetList.set(i, "*");
       } else {
         remainingCharList.add(userGuessArray.get(i));
       }
     }
-    System.out.println("FIRST LOOP CC " + colourCode);
-    System.out.println(" REMAINING CHAR LIST " + remainingCharList + " TarList " + targetList);
+  }
 
-    //Loop to identify present, but wrong place
-    for (int i = 0; i< userGuessArray.size(); i++) {
-      if ((colourCode.get(i) == 2) && targetList.contains(userGuessArray.get(i))) {
+  void setOrangeTiles(ArrayList<String> userGuessArray, ArrayList<String> targetList) {
+    // Loop to identify present, but wrong place
+    for (int i = 0; i < userGuessArray.size(); i++) {
+      String number = userGuessArray.get(i);
+      boolean correct = colourCode.get(i) == 0;
+      if (!correct && targetList.contains(number)) {
         colourCode.set(i, 1);
         remainingCharList.remove(userGuessArray.get(i));
         targetList.remove(userGuessArray.get(i));
-        System.out.println("i " + i + "CC " + colourCode + " RemCharL " + remainingCharList + " TarList " + targetList);
       }
     }
-    System.out.println("SECOND LOOP CC " + colourCode);
-
-  System.out.println(colourCode);
-    return colourCode;
   }
 
   @Override
   public boolean hasWon() {
-    return colourCode.contains(1) || (colourCode.contains(2));
+    if (colourCode == null) {
+      return false;
+    } else {
+      return !(colourCode.contains(1) || (colourCode.contains(2)));
+    }
   }
 
   @Override
@@ -81,6 +94,7 @@ public class Game implements GameInterface {
   }
 
   @Override
-  public String getTargetResult() { return targetResult; }
-
+  public String getTargetResult() {
+    return targetResult;
+  }
 }
