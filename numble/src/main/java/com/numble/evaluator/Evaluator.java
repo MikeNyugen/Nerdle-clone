7bc86784b2a981c21b27d77eb1291da262dbffd4
Dmitry Voronetskiy <davoronetskiy@gmail.com>
2022-03-03 22:14:00 +0000

    added evaluator of equations

---
+++ numble/src/main/java/com/numble/evaluator/Evaluator.java
@@ -0,0 +1,154 @@
+package com.numble.evaluator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evaluator {
+    static private boolean isNumber(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    static private Integer combine(Number left, Operation operation, Number right) {
+        switch (operation) {
+            case PLUS: return left.number + right.number;
+            case MUL: return left.number * right.number;
+            case DIV: return left.number / right.number;
+            case MINUS: return left.number - right.number;
+            default: throw new RuntimeException("unreachable code reached, probably bad equation used");
+        }
+    }
+
+    private interface EvaluatorToken {
+    }
+
+    private static class Number implements EvaluatorToken {
+        public Integer number;
+
+        Number(int a) {
+            number = a;
+        }
+    }
+
+    private enum Operation implements EvaluatorToken {
+        PLUS,
+        MINUS,
+        MUL,
+        DIV;
+
+        static Operation fromChar(char c) {
+            switch (c) {
+                case '+': return PLUS;
+                case '-': return MINUS;
+                case 'x': return MUL;
+                case '/': return DIV;
+                default: throw new RuntimeException("unreachable code reached, probably bad equation used");
+            }
+        }
+    }
+
+    private enum Bracket implements EvaluatorToken {
+        OPENING,
+        CLOSING;
+
+        static Bracket fromChar(char c) {
+            switch (c) {
+                case '(': return OPENING;
+                case ')': return CLOSING;
+                default: throw new RuntimeException("unreachable code reached, probably bad equation used");
+            }
+        }
+    }
+
+    static private boolean isBracket(char c) {
+        return c == '(' || c == ')';
+    }
+
+    static private List<EvaluatorToken> tokenize(String equation) {
+        List<EvaluatorToken> result = new ArrayList<>();
+        int i = 0;
+        while (i < equation.length()) {
+            if (isNumber(equation.charAt(i))) {
+                int number = 0;
+                while (i < equation.length() && isNumber(equation.charAt(i))) {
+                    number *= 10;
+                    number += (equation.charAt(i) - '0');
+                    i++;
+                }
+                result.add(new Number(number));
+            } else if (isBracket(equation.charAt(i))) {
+                result.add(Bracket.fromChar(equation.charAt(i)));
+                i++;
+            } else {
+                result.add(Operation.fromChar(equation.charAt(i)));
+                i++;
+            }
+        }
+        return result;
+    }
+
+    static public Integer evaluate(String equation) {
+        var tokens = tokenize(equation);
+        return evaluate(tokens).number;
+    }
+
+    static public Number evaluate(List<EvaluatorToken> tokens) {
+        int last_token_index = tokens.size() - 1;
+        if (tokens.get(last_token_index) instanceof Bracket) {
+            int i = last_token_index;
+            int balance = 1;
+            while (balance != 0) {
+                i--;
+                if (tokens.get(i) instanceof Bracket && tokens.get(i) == Bracket.CLOSING) {
+                    balance++;
+                } else if (tokens.get(i) instanceof Bracket && tokens.get(i) == Bracket.OPENING) {
+                    balance--;
+                }
+            }
+            var last_bracket = evaluate(tokens.subList(i + 1, last_token_index));
+            tokens = tokens.subList(0, i);
+            tokens.add(last_bracket);
+            return evaluate(tokens);
+        } else {
+            // last token is a number
+            if (tokens.size() == 1) {
+                return (Number) tokens.get(0);
+            }
+            Number last_number = (Number) tokens.get(last_token_index);
+            Operation operation = (Operation) tokens.get(last_token_index - 1);
+            return new Number(combine(evaluate(tokens.subList(0, last_token_index - 1)), operation, last_number));
+        }
+    }
+/*
+    static public Integer badEvaluate(String equation) {
+        if (equation.length() == 0) {
+            return 0;
+        } else if (isNumber(equation.charAt(0))) {
+            // equation starts with a number
+            int first_number = 0;
+            int i = 0;
+            while (i < equation.length() && isNumber(equation.charAt(i))) {
+                first_number *= 10;
+                first_number += (equation.charAt(i) - '0');
+                i++;
+            }
+            if (i == equation.length()) {
+                return first_number;
+            } else {
+                return combine(first_number, equation.charAt(i), badEvaluate(equation.substring(i + 1)));
+            }
+        } else {
+            if (equation.charAt(0) != '(') {
+                throw new RuntimeException("unreachable code reached, probably bad equation used");
+            }
+            int i = 0;
+            while (i < equation.length() && equation.charAt(i) != ')') {
+                i++;
+            }
+            if (i == equation.length()) {
+                throw new RuntimeException("unreachable code reached, probably bad equation used");
+            }
+            return combine(badEvaluate(equation.substring(1, i)), equation.charAt(i + 1), badEvaluate(equation.substring(i + 2)));
+        }
+    }
+ */
+}
