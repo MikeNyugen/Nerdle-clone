bbbe3604668ed84e133622ba078e76ea0e0b480e
Dmitry Voronetskiy <davoronetskiy@gmail.com>
2022-03-06 11:15:58 +0000

    minor

--- numble/src/main/java/com/numble/NumbleApplication.java
+++ numble/src/main/java/com/numble/NumbleApplication.java
@@ -1,12 +1,10 @@
 package com.numble;
 
-import com.numble.UI.GameView;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NumbleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NumbleApplication.class, args);
 	}
